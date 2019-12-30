185358e67e1b5eaf1c5f3bf0b58081ff221a6124
ownerofworld <43425662+ownerofworld@users.noreply.github.com>
2019-12-30 22:45:22 +0530

    Add files via upload

---
+++ index(1).php
@@ -0,0 +1,33 @@
+<?php 
+
+
+$website = "https://api.telegram.org/bot939549218:AAH-_DX0ZIAO6oz-E-i-SYKS15aFmui0ZnI";
+
+
+$request = file_get_contents( 'php://input' );
+#          ↑↑↑↑ 
+$request = json_decode( $request, TRUE );
+
+$chatId = $request["message"]["chat"]["id"];
+
+sendMessage($chatId, "Hello World", $token);
+
+
+
+function sendMessage($chatID, $messaggio, $token) {
+    echo "sending message to " . $chatID . "\n";
+
+    $url = "https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chatID;
+    $url = $url . "&text=" . urlencode($messaggio);
+    $ch = curl_init();
+    $optArray = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true
+    );
+    curl_setopt_array($ch, $optArray);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
+
+ ?>
