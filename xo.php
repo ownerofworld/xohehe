6acb47da2e826cc047fa1c57c2ed5af1db44eb5b
ownerofworld <43425662+ownerofworld@users.noreply.github.com>
2020-03-02 23:48:19 +0530

    Update xo.php

--- xo.php
+++ xo.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BOT_TOKEN', 'Your Token Here');
+define('BOT_TOKEN', '816624660:AAGYGbmQXS2atlvDhn2rvAe9TP0dmYVAF7g');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
